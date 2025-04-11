df19fad098c1fd5c85502409c468a8905b0df64c
Jon <app@therevolv.com>
2025-04-11 00:11:25 -0400

    Create page.tsx

---
+++ app/appclip/test-launch/page.tsx
@@ -0,0 +1,8 @@
+export default function AppClipLaunch() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>🚀 App Clip Triggered!</h1>
+      <p>This page was loaded via universal link.</p>
+    </div>
+  );
+}
