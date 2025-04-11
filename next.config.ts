7534d2e067f64fd17fc46e3d9446ce79cb828df4
Jon <app@therevolv.com>
2025-04-11 00:00:00 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json'
+          }
+        ]
+      }
+    ]
+  }
+}
